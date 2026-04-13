fde82337cfafb5fe75ae18f63bf584477b10a962
mohanad <mohanad730205170@gmail.com>
2026-04-13 23:12:55 +0700

    Revert API URL to localhost

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://recipe-platform-production-3dfd.up.railway.app/api'
+  baseURL: 'http://localhost:5000/api'
 });
 
 API.interceptors.request.use((config) => {
